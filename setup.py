0bd1c524568e5ef9ea7923159b1cc3ce5bf0cef0
Francesco Regazzoni <francesco.regazzoni@polimi.it>
2021-07-08 10:23:32 +0200

    Added setuptools support.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import io
+from setuptools import setup
+from setuptools import find_packages
+
+with open("requirements.txt", "r") as f:
+    requirements = [line.strip() for line in f.readlines()]
+
+exec(open('cardioemulator/_version.py').read())
+
+setup(
+    name = "cardioemulator",
+    version = __version__,
+    description = "Library implementing a 0D emulator of cardiac 3D electromechanical models.",
+    author="Francesco Regazzoni",
+    author_email = "francesco.regazzoni@polimi.it",
+    license = "MIT",
+    install_requires = requirements,
+    packages = find_packages(),
+    scripts = [
+        'apps/cardioemulator_build',
+        'apps/cardioemulator_build_parametric'
+        ],
+)
